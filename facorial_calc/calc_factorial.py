3c47c1339e20bec12c3aeb64c826e6243abe848a
Adnaan Zubair Shah <136037246+adnaanzshah@users.noreply.github.com>
2024-09-20 19:22:02 +0530

    made changes in factorial file

--- facorial_calc/calc_factorial.py
+++ facorial_calc/calc_factorial.py
@@ -1,11 +1,21 @@
 # Just a Demo Self Written Code for calculating factorial of any given number
 
-num=10
+# num=10
 # num=int(input("Enter a number : "))
-fact=1
-factorial=1
-while(fact<num):
-    factorial=fact*factorial
-    fact=fact+1
+def factorial(num):
+    fact=1
+    factorial=1
+    while(fact<(num+1)):
+        factorial=fact*factorial
+        fact=fact+1
+        print(f"{num}! = ",factorial)
+    return (f"{num}! = ",factorial)
 
-print(f"{num}! = ",factorial)
+factorial(4)
+
+n=5
+def factorial_recursion(n):
+    if (n==1 or n==0):
+        return 1
+    return(n*factorial_recursion(n-1))
+print(factorial_recursion(5))
